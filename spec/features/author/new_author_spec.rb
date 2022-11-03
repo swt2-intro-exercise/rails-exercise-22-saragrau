58ca046d2b359f1058b8bd9eabfcc6e330641464
Sara Grau <grauiblade@uni-potsdam.de>
2022-11-03 12:00:33 +0100

    #4

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -7,10 +7,19 @@ describe "New author page", type: :feature do
   end
 
   it "should have text inputs for an author's first name, last name, and homepage" do
-	visit new_author_path
-	# these are the standard names given to inputs by the Rails form builder
-	expect(page).to have_field('author[first_name]')
-	expect(page).to have_field('author[last_name]')
-	expect(page).to have_field('author[homepage]')
+	  visit new_author_path
+	  # these are the standard names given to inputs by the Rails form builder
+	  expect(page).to have_field('author[first_name]')
+	  expect(page).to have_field('author[last_name]')
+	  expect(page).to have_field('author[homepage]')
+  end
+
+  it "should fill in the text inputs and submit them" do
+    visit new_author_path
+    page.fill_in 'author[first_name]', with: 'Edsger'
+    page.fill_in 'author[last_name]', with: 'Dijkstra'
+    page.fill_in 'author[homepage]', with: 'https://en.wikipedia.org/wiki/Edsger_W._Dijkstra'
+    
+    find('input[type="submit"]').click
   end
 end
