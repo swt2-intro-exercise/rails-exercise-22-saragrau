530867e9b6644f0deadbba2cf8a9e1862f476cf3
Sara Grau <grauiblade@uni-potsdam.de>
2022-11-03 11:44:14 +0100

    Failing test for #3

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_02_180013) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_03_102145) do
   create_table "authors", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "homepage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
