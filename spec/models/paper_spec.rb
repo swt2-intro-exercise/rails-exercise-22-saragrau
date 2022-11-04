dfb95050531740b4086dcdf3155d1a212e67c6f9
Sara Grau <grauiblade@uni-potsdam.de>
2022-11-04 15:30:45 +0100

    #10

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should exist a Paper Model" do
+    paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: 1950)
+    expect(paper.title).to eq("COMPUTING MACHINERY AND INTELLIGENCE")
+    expect(paper.venue).to eq("Mind 49: 433-460")
+    expect(paper.year).to eq(1950)
+  end
+
+  it "should fail when creating a paper without a title" do
+    paper = Paper.new(venue: "Mind 49: 433-460", year: 1950)
+    expect(paper).to_not be_valid
+  end
 end
