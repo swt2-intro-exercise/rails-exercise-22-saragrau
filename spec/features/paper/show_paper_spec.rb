bc8928f14694754850a0bce534a145ac26690794
Sara Grau <grauiblade@uni-potsdam.de>
2022-11-08 12:00:58 +0100

    #17

---
+++ spec/features/paper/show_paper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "Show paper page", type: :feature do
+	it "shows the paper's authors" do
+		@paper = FactoryBot.create :paper
+		#@author = FactoryBot.create :author
+		#@paper.authors.create({first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.de/Alan_Turing"})
+		visit paper_path(@paper)
+		@paper.authors.each do |author|
+			expect(page).to have_text(author.name)
+		end
+	end
+end
+
+
